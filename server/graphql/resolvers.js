4b6ebf36a1f6e7413441b3c997f4727d7597929b
aronresni <aronresni@gmail.com>
2024-03-15 11:13:25 -0300

    model-task

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,5 +1,5 @@
 import Project from "../models/Project.js";
-
+import Task from "../models/Task.js"
 export const resolvers = {
     Query: {
         hello: () => "Hello World",
@@ -7,7 +7,10 @@ export const resolvers = {
             // Utiliza await para esperar a que la consulta a la base de datos se complete
             const projects = await Project.find();
             return projects;
-        }
+        },
+        project: async (_, { _id }) => await Project.findById(_id),
+        task: async (_, { _id }) => await Task.findById(_id),
+        tasks: async () => await Task.find(),
     },
     Mutation: {
         createProject: async (_, { name, description }) => {
@@ -21,6 +24,20 @@ export const resolvers = {
                 return savedProject;
             } catch (error) {
                 // Maneja cualquier error que pueda ocurrir durante la operación de guardado
+                throw new Error(`Error al crear el proyecto: ${error.message}`);
+            }
+        },
+        createTask: async (_, { title, projectId }) => {
+            try {
+                const projectFound = await Project.findById(projectId)
+                if (!projectFound) throw new Error()
+                const task = new Task({
+                    title,
+                    projectId,
+                });
+                const taskSaved = await task.save();
+                return taskSaved;
+            } catch (error) {
                 throw new Error(`Error al crear el proyecto: ${error.message}`);
             }
         }
--- server/models/Task.js
+++ server/models/Task.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
-new mongoose.Schema({
-    tittle: {
+const TaskSchema = new mongoose.Schema({
+    title: { // Fixed typo: 'tittle' to 'title'
         type: String,
         required: true
     },
     projectId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: `Project`,
+        ref: "Project", // Fixed backtick to regular quotes
         required: true,
     }
-},
-    {
-        timestamps: true,
-    })
-export default mongoose.model("Task")
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model("Task", TaskSchema);
+
